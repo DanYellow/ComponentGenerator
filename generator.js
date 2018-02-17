cfaffcdb5746d38eca2b0acf17d4dfa2be234583
DanYellow <jeanlouis.danielo@sfr.fr>
2018-02-17 01:48:44 -0500

    Adds manager in a better way reduxed components

--- generator.js
+++ generator.js
@@ -1,88 +1,21 @@
 const fs = require('fs');
-const inquirer = require('inquirer');
 const toPascalCase = require('to-pascal-case');
 const Handlebars = require('handlebars');
 const util = require('util');
 const path = require('path');
 
+Handlebars.registerHelper('toLowerCase', (str) => 
+    str.toLowerCase()
+);
+
+Handlebars.registerHelper('toUpperCase', (str) =>
+    str.toUpperCase()
+);
+
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 
-const questionListFiller = ({name, choices, message, preselected}) => ({
-    type: 'list',
-    name,
-    message,
-    choices,
-    default: preselected,
-    filter: (val) => {
-        return val.toLowerCase();
-    }
-})
-
-
-const questionConfirmFiller = ({name, message, preselected = false}) => ({
-    type: 'confirm',
-    name,
-    message,
-    default: preselected
-})
-
-const fileTypeQuestion = () => {
-    const question = questionListFiller({
-        name: 'type',
-        message: 'What kind of new file you want to create?',
-        choices: ['Component', 'Container', 'Page'],
-        preselected: 'Component',
-    })
-    return inquirer.prompt([question])
-}
-
-const secondQuestion = (answer) => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: `What's your ${answer.type}'s name?`,
-            validate: function(value) {
-              var pass = value.match(
-                /^[a-z\-]\S{3,}$/
-              );
-              if (pass) {
-                return true;
-              }
-        
-              return `
-                Please enter a valid ${answer.type} name.
-                Spaces, numbers, underscore and specials chars are not allowed
-                `;
-            },
-            filter: (val) => {
-                return val.toLowerCase();
-            }
-        }
-    ])
-}
-
-const thirdQuestion = (answer) => {
-    let question = null;
-    switch(answer.type) {
-        case 'component':
-            question = questionConfirmFiller({
-                message: 'Is a staless component?',
-                name: 'is_stateless',
-            })
-        break;
-        case 'page':
-        case 'container':
-            question = questionConfirmFiller({
-                message: 'Does he need redux?',
-                name: 'needs_redux',
-            })
-        break;
-    }
-    question.preselected = 'Y'
-    return inquirer.prompt([question])
-}
+const questions = require('./questions')
 
 const getTplNameForType = (type, isStateless) => {
     switch(true) {
@@ -103,13 +36,17 @@ const getTplNameForType = (type, isStateless) => {
 
 let allAnswers = {};
 const generator = async () => {
-    const fileTypeAnwser = await fileTypeQuestion();
+    const fileTypeAnwser = await questions.fileTypeQuestion();
     allAnswers = {...allAnswers, ...fileTypeAnwser};
-    const secondAnwser = await secondQuestion(allAnswers);
-    allAnswers = {...allAnswers, ...secondAnwser};
-    const thirdAnwser = await thirdQuestion(allAnswers);
-    allAnswers = {...allAnswers, ...thirdAnwser};
+    const filenameAnwser = await questions.filenameQuestion(allAnswers);
+    allAnswers = {...allAnswers, ...filenameAnwser};
+    const componentSpecifityAnswer = await questions.componentSpecifityQuestion(allAnswers);
+    allAnswers = {...allAnswers, ...componentSpecifityAnswer};
 
+    if (allAnswers.needs_redux) {
+        const reduxComplexityAnswer = await questions.reduxComplexityQuestion(allAnswers);
+        allAnswers = {...allAnswers, ...reduxComplexityAnswer};
+    }
 
     const fileFolder = `./${allAnswers.name}`;
     if (!fs.existsSync(fileFolder)){
@@ -122,15 +59,30 @@ const generator = async () => {
     const tpls = [
         {
             path: `./templates/${templateType}/index.hbs`,
+            dist: `${fileFolder}`,
             distExt: 'jsx'
         }, {
             path: `./templates/shared/index.test.hbs`,
+            dist: `${fileFolder}`,
             distExt: 'js'
         }, {
             path: `./templates/shared/style.hbs`,
+            dist: `${fileFolder}`,
             distExt: 'pcss' // dynamise ?
         }
-    ]
+    ];
+
+    if (allAnswers.needs_redux) {
+        tpls.push({
+            path: `./templates/container/reducer.hbs`,
+            distExt: 'js',
+            dist: `${fileFolder}/modules`,
+            distName: 'index'
+        })
+
+        fs.mkdirSync(`${fileFolder}/modules`);
+    }
+
     const promises = []
     const pascalCaseName = toPascalCase(allAnswers.name);
     tpls.forEach(async (tpl) => {
@@ -140,10 +92,11 @@ const generator = async () => {
             ...allAnswers,
             name: pascalCaseName,
         });
+        const distName = (tpl.distName) ? tpl.distName : path.basename(tpl.path, '.hbs')
 
         promises.push(
             writeFile(
-                `${fileFolder}/${path.basename(tpl.path, '.hbs')}.${tpl.distExt}`, 
+                `./${tpl.dist}/${distName}.${tpl.distExt}`, 
                 result, 
                 'utf8'
             )
